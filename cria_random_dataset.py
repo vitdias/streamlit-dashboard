6f58d64c05e92a867ca125dcc809535a9d693e92
vitdias <vitor.azambuja.90@gmail.com>
2021-10-23 03:17:35 -0300

    Random dataset ajeitado para o medium
    Ajeitado o programa cria_random_dataset.py para se adequar ao artigo que está sendo escrito no medium

--- cria_random_dataset.py
+++ cria_random_dataset.py
@@ -1,75 +1,74 @@
+# Importa as Libs
 import numpy as np
 import pandas as pd
 import time
 from datetime import datetime, timedelta
 import warnings
 
-warnings.filterwarnings(action="ignore", module="scipy",
-                        message="^internal gelsd")
+# Desativa os warnings chatos que vão aparecer no log devido ao método random_integers
+warnings.filterwarnings(action="ignore")
 
-df = pd.DataFrame(np.random.random_integers(20, size=(1000, 16)),
-    columns=['agencia', 'decisão', 'biocatch_IsBot', 'biocatch_IsMalware', 'biocatchIsRat', 'canal', 'DT_REF',
-             'device_diebold_tag', 'público', 'segmento', 'tipoContaBeneficiario', 'tipoContaPagador',
-             'tipoTransacao',
-             'transacao_Login', 'Transacao_pix', 'qtd'])
+# Cria o dataframe com números randomicos, já nomeando as colunas. No exemplo abaixo, eu crio 9 colunas com 10mil linhas cada que vão receber valores randômicos de 1 a 20
+# Estou fazendo isso só para entenderem melhor como funciona os atributos do np.random.random_integers(), pq na verdade eu só precisava fazer isso para a variável agencia e DT_PROPOSTA
+df = pd.DataFrame(np.random.random_integers(20, size=(10000, 9)),
+    columns=['agencia', 'decisao', 'canal', 'DT_PROPOSTA', 'regra', 'publico', 'segmento', 'cheque_especial', 'qtd'])
 
+# Adiciona novos números randômicos para cada variável, dependendo do que queremos para cada uma delas
+df['decisao'] = np.random.random_integers(3, size=(10000, 1))
+df['canal'] = np.random.random_integers(6, size=(10000, 1))
+df['regra'] = np.random.random_integers(10, size=(10000, 1))
+df['publico'] = np.random.random_integers(2, size=(10000, 1))
+df['segmento'] = np.random.random_integers(4, size=(10000, 1))
+df['cheque_especial'] = np.random.random_integers(2, size=(10000, 1))
+df['qtd'] = np.random.random_integers(7000, size=(10000, 1))
 
-df['decisão'] = np.random.random_integers(3, size=(1000, 1))
-df['biocatch_IsBot'] = np.random.random_integers(2, size=(1000, 1))
-df['biocatch_IsMalware'] = np.random.random_integers(2, size=(1000, 1))
-df['biocatchIsRat'] = np.random.random_integers(2, size=(1000, 1))
-df['canal'] = np.random.random_integers(7, size=(1000, 1))
-df['device_diebold_tag'] = np.random.random_integers(10, size=(1000, 1))
-df['público'] = np.random.random_integers(3, size=(1000, 1))
-df['segmento'] = np.random.random_integers(20, size=(1000, 1))
-df['tipoContaBeneficiario'] = np.random.random_integers(4, size=(1000, 1))
-df['tipoContaPagador'] = np.random.random_integers(4, size=(1000, 1))
-df['tipoTransacao'] = np.random.random_integers(10, size=(1000, 1))
-df['transacao_Login'] = np.random.random_integers(2, size=(1000, 1))
-df['Transacao_pix'] = np.random.random_integers(2, size=(1000, 1))
-df['qtd'] = np.random.random_integers(7000, size=(1000, 1))
-
-
-df['decisão'] = df['decisão'].astype(str)
-df['decisão'] = df['decisão'].replace(['1', '2', '3'],['APROVAR', 'NEGAR', 'DERIVAR'])
-
-df['biocatch_IsBot'] = df['biocatch_IsBot'].astype(str)
-df['biocatch_IsBot'] = df['biocatch_IsBot'].replace(['1', '2'],['TRUE', 'FALSE'])
-
-df['biocatch_IsMalware'] = df['biocatch_IsMalware'].astype(str)
-df['biocatch_IsMalware'] = df['biocatch_IsMalware'].replace(['1', '2'],['TRUE', 'FALSE'])
-
-df['biocatchIsRat'] = df['biocatchIsRat'].astype(str)
-df['biocatchIsRat'] = df['biocatchIsRat'].replace(['1', '2'],['TRUE', 'FALSE'])
+# Substitui os números criados anteriormente pelas respostas desejadas em cada variável
+df['decisao'] = df['decisao'].astype(str)
+# df['decisao'] = df['decisao'].replace(['1', '2', '3'],['APROVAR', 'NEGAR', 'DERIVAR'])
 
 df['canal'] = df['canal'].astype(str)
-df['canal'] = df['canal'].replace(['1', '2', '3', '4', '5', '6', '7'],['BCD', 'CDF', 'IBF', 'IBJ', 'MBF', 'MBJ', 'SPD'])
+df['canal'] = df['canal'].replace(['1', '2', '3', '4', '5', '6'],['Internet Banking PF', 'Internet Banking PJ', 'Mobile Banking PF', 'Mobile Banking PJ', 'Agência Física PF', 'Agência Física PJ'])
 
-df['device_diebold_tag'] = df['device_diebold_tag'].astype(str)
-df['device_diebold_tag'] = df['device_diebold_tag'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],['app_acesso_remoto', 'sfr_acesso_nrecomendado', 'sfr_acesso_nrecomendado_v2', 'sfr_device_recorrente_sup30d', 'sfr_device_recorrente_ate30d', 'sfr_device_recorrente_sup60d', 'sfr_device_recorrente_ate60d', 'sfr_device_recorrente_sup90d', 'sfr_device_recorrente_ate90d', 'novo_usuario'])
+df['regra'] = df['regra'].astype(str)
+df['regra'] = df['regra'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'])
 
-df['público'] = df['público'].astype(str)
-df['público'] = df['público'].replace(['1', '2', '3'],['ND', 'PF', 'PJ'])
+df['segmento'] = df['segmento'].astype(str)
+df['segmento'] = df['segmento'].replace(['1', '2', '3', '4'],['Altíssima Renda', 'Alta Renda', 'Média Renda', 'Massificado'])
 
-df['tipoContaBeneficiario'] = df['tipoContaBeneficiario'].astype(str)
-df['tipoContaBeneficiario'] = df['tipoContaBeneficiario'].replace(['1', '2', '3', '4'],['Conta Corrente', 'Conta Poupança', 'Conta Salário', 'Conta Pagamento'])
+df['DT_PROPOSTA'] = df['DT_PROPOSTA'].astype(str)
+df['DT_PROPOSTA'] = df['DT_PROPOSTA'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],['01/09/2021', '02/09/2021', '03/09/2021', '04/09/2021', '05/09/2021', '06/09/2021', '07/09/2021', '08/09/2021', '09/09/2021', '10/09/2021', '11/09/2021', '12/09/2021', '13/09/2021', '14/09/2021', '15/09/2021', '16/09/2021', '17/09/2021', '18/09/2021', '19/09/2021', '20/09/2021'])
+df['DT_PROPOSTA'] = pd.to_datetime(df['DT_PROPOSTA'], format='%d/%m/%Y')
 
-df['tipoContaPagador'] = df['tipoContaPagador'].astype(str)
-df['tipoContaPagador'] = df['tipoContaPagador'].replace(['1', '2', '3', '4'],['Conta Corrente', 'Conta Poupança', 'Conta Salário', 'Conta Pagamento'])
+df['cheque_especial'] = df['cheque_especial'].astype(str)
+df['cheque_especial'] = df['cheque_especial'].replace(['1', '2'],['TRUE', 'FALSE'])
 
-df['tipoTransacao'] = df['tipoTransacao'].astype(str)
-df['tipoTransacao'] = df['tipoTransacao'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],['CONSULTA_SENHA_TSC', 'HABILITA_SENHA', 'LOGIN_SUCESSO', 'OPBK_CONSENT_APROV', 'PAGAMENTO', 'PAGAMENTO_DDA', 'PAGAMENTO_DDA', 'PIX_CONFIRMAR_PORTAB', 'PIX_INCLUIR_CHAVE', 'PIX_PAGAMENTO_CELULAR'])
+# Aqui eu crio outras variáveis utilizando outro método randômico, só pra mostrart outra forma que  também funciona
+df['Score_A'] = np.random.random_integers(550, size=(10000, 1))
+df['Score_B'] = np.random.random_integers(110, size=(10000, 1))
 
-df['DT_REF'] = df['DT_REF'].astype(str)
-df['DT_REF'] = df['DT_REF'].replace(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],['01/09/2021', '02/09/2021', '03/09/2021', '04/09/2021', '05/09/2021', '06/09/2021', '07/09/2021', '08/09/2021', '09/09/2021', '10/09/2021', '11/09/2021', '12/09/2021', '13/09/2021', '14/09/2021', '15/09/2021', '16/09/2021', '17/09/2021', '18/09/2021', '19/09/2021', '20/09/2021'])
-df['DT_REF'] = pd.to_datetime(df['DT_REF'], format='%d/%m/%Y')
+# Aqui eu começo a criar variáveis com condições
+df['TimeOut_A'] = np.where(df['Score_A'] > 500, 'TimeOut', 'Consulta OK')
+df['TimeOut_B'] = np.where(df['Score_B'] > 100, 'TimeOut', 'Consulta OK')
 
-df['transacao_Login'] = df['transacao_Login'].astype(str)
-df['transacao_Login'] = df['transacao_Login'].replace(['1', '2'],['TRUE', 'FALSE'])
+keep_canal = ['Internet Banking PF', 'Mobile Banking PF', 'Agência Física PF']
 
-df['Transacao_pix'] = df['Transacao_pix'].astype(str)
-df['Transacao_pix'] = df['Transacao_pix'].replace(['1', '2'],['TRUE', 'FALSE'])
+df['publico'] = df['canal'].isin(keep_canal)
+df['publico'] = np.where(df['publico'] == True, "PF", "PJ")
 
-print(df)
+keep_neg = ['01', '02', '03', '04', '05']
+keep_drv = ['06', '07', '08', '09']
 
-df.to_csv(r'C:\Users\vitor\Desktop\Python Programs\Dash com Streamlit\Lib\base_dataset2.csv')
+df['decisao'] = np.where(df['regra'].isin(keep_neg), 'NEGAR', df['decisao'])
+df['decisao'] = np.where(df['regra'].isin(keep_drv), 'DERIVAR', df['decisao'])
+df['decisao'] = np.where(df['regra'] == '10', 'APROVAR', df['decisao'])
+
+df['Limite'] = np.where(df['decisao'] == 'APROVAR', np.random.randint(500, 2000, size=(10000)) * df['qtd'], 0)
+
+df['lat'] = np.random.randn(10000) / 5 + -23.53
+df['lon'] = np.random.randn(10000) / 5 + -46.62
+
+# print(df)
+
+# E por fim, aqui eu crio um csv em uma pasta local
+df.to_csv(r'C:\Users\vitor\Documents\GitHub\streamlit-dashboard\base_dataset2.csv')
+print('Rodei')
